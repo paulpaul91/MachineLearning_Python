b14a2540027aaf84f9f0c4288ea4a5950bb3fd17
peterchenyijie <2232941357@qq.com>
2017-12-19 23:57:48 +0800

    import numpy as np

--- K-Means/K-Means_scikit-learn.py
+++ K-Means/K-Means_scikit-learn.py
@@ -8,7 +8,7 @@ from sklearn.cluster import KMeans
 def kMenas():
     data = spio.loadmat("data.mat")
     X = data['X']   
-    model = KMeans(n_clusters=3).fit(X) # n_clusters指定3类，拟合数据
+    model = KMeans(n_clusters=3).fit(X)  # n_clusters指定3类，拟合数据
     centroids = model.cluster_centers_  # 聚类中心
     
     plt.scatter(X[:,0], X[:,1])     # 原数据的散点图
